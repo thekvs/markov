d94267639adf2ffc959bee1664180091011608b5
Konstantin Sorokin <kvs@sigterm.ru>
2013-10-29 21:39:48 +0400

    check for errors while opening files

--- serialize.hpp
+++ serialize.hpp
@@ -9,13 +9,18 @@
 #include <boost/serialization/vector.hpp>
 #include <boost/serialization/map.hpp>
 
+#include "exc.hpp"
+
 namespace markov {
 
 template <typename T>
 void save(const T &s, std::string filename)
 {
     std::ios_base::openmode mode = std::ios::out | std::ios::binary | std::ios::trunc;
+
     std::ofstream ofs(filename.c_str(), mode);
+    THROW_EXC_IF_FAILED(!ofs.fail(), "couldn't open file %s", filename.c_str());
+
     boost::archive::binary_oarchive oa(ofs);
 
     oa << s;
@@ -25,8 +30,11 @@ template <typename T>
 void load(T &s, std::string filename)
 {
     std::ios_base::openmode mode = std::ios::in | std::ios::binary;
-    std::ifstream ifs(filename.c_str());
-    boost::archive::binary_iarchive ia(ifs, mode);
+    
+    std::ifstream ifs(filename.c_str(), mode);
+    THROW_EXC_IF_FAILED(!ifs.fail(), "couldn't open file %s", filename.c_str());
+
+    boost::archive::binary_iarchive ia(ifs);
 
     ia >> s;
 }
