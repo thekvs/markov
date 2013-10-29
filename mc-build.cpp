9e4050119b447aff4d43cfdf44a58be5f6d7804e
Konstantin Sorokin <kvs@sigterm.ru>
2013-10-29 19:38:34 +0400

    reformatting

--- mc-build.cpp
+++ mc-build.cpp
@@ -14,7 +14,7 @@ static struct option longopts[] = {
     { "dimension",  required_argument,  NULL,   'd' },
     { "model",      required_argument,  NULL,   'm' },
     { "help",       no_argument,        NULL,   'h' },
-    { NULL,     0,                  NULL,   0   }
+    { NULL,         0,                  NULL,   0   }
 };
 
 struct Args {
