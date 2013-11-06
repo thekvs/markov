5f6866fc9d5b30cad2d15b53908ac456d813080f
Konstantin Sorokin <kvs@sigterm.ru>
2013-11-06 15:43:12 +0400

    refactored

--- exc.hpp
+++ exc.hpp
@@ -13,36 +13,28 @@ namespace markov {
 class Exc: public std::exception {
 public:
 
-    Exc(const char *file, int line, const char *fmt, ...): msg(NULL)
+    Exc(const char *file, int line, const char *fmt, ...)
     {
         va_list ap;
         char    buff[1024];
-        size_t  len;
 
         va_start(ap, fmt);
         vsnprintf(buff, sizeof(buff), fmt, ap);
         va_end(ap);
 
-        len = 0;
-        memset(line_str, 0, sizeof(line_str));
-        snprintf(line_str, sizeof(line_str), "%i", line);
-        len = strlen(buff) + 4 /* ' at ' */ + strlen(file) + 1 /* ':' */ + strlen(line_str) + 1 /* terminating zero */;
-        msg = static_cast<char*>(calloc(1, len));
-        snprintf(msg, len, "%s at %s:%s", buff, sanitize_file_name(file), line_str);
+        snprintf(msg, sizeof(msg), "%s:%i %s", sanitize_file_name(file), line, buff);
     }
 
     virtual ~Exc() throw() {
-        free(msg);
     };
 
     virtual const char *what() const throw() {
-        return msg == NULL ? "" : msg;
+        return msg;
     }
 
 private:
 
-    char *msg;
-    char  line_str[sizeof("2147483647")];
+    char msg[1024];
 
     Exc() {}
 
