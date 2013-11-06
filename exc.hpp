b7f71282132367b655825fe1243f9df3a3055b06
Konstantin Sorokin <kvs@sigterm.ru>
2013-11-06 19:52:20 +0400

    copy ctor in exception class.

--- exc.hpp
+++ exc.hpp
@@ -31,6 +31,10 @@ public:
         snprintf(msg, sizeof(msg), "%s:%i %s", sanitize_file_name(file), line, buff);
     }
 
+    Exc(const Exc &exc) {
+        strncpy(msg, exc.what(), sizeof(msg));
+    }
+
     virtual ~Exc() throw() {
     }
 
